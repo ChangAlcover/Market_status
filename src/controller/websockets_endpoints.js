bd008bb30b19597c594d79d0479729dedd75bee5
ChangAlcover <65588554+ChangAlcover@users.noreply.github.com>
2022-03-27 22:14:54 -0300

    Update websockets_endpoints.js
    Separate further the logic of calculating the effective price for a purchase, creating a function without promises.

--- src/controller/websockets_endpoints.js
+++ src/controller/websockets_endpoints.js
@@ -13,52 +13,57 @@ export function retrieve_bid (pair_name, base_wss) {
 	});
 }
 
-
 export function retrieve_efect_price (pair_name,operation,amount,limit_price, base_wss) {
 	const depth=25;
 	return new Promise (function(res, rej) {
 		endpoints_conections.retrieve_book(pair_name,depth,base_wss)
 		.then(response => {
-			var amount_limit=0;
-			var amount_limit_value=0;
-			var acumulate=0;
-			var acumulate_value=0;
-			var data=[];
-
-			if (operation==='sell')	{
-				data=response[0];
-			}
-			if (operation==='buy')	{
-				data=response[1];
-			}
-			console.log(data);
-
-			for (let i = 0; i <depth; i++) {
-				if (amount-acumulate >0) {
-					if (amount-acumulate > data[i][1]) {
-						acumulate_value+=data[i][0]*data[i][1];
-					}else {
-						acumulate_value+=data[i][0]*(amount-acumulate);
-					}
-					acumulate+=data[i][1];
-				}
-				
-				if (limit_price<data[i][0] && operation==='sell') {
-					amount_limit+=data[i][1];
-					amount_limit_value+=data[i][0]*data[i][1];
-					}
-				if (limit_price>data[i][0] && operation==='buy') {
-					amount_limit+=data[i][1];
-					amount_limit_value+=data[i][0]*data[i][1];
-					}
-			}
-			res([acumulate_value,amount_limit,amount_limit_value]);
+			const data = response;
+			res(efect_price_logic(operation,amount,limit_price,depth,data));
 		})
-		.catch(err => {
+		.catch (err => {
 			console.log("Error in function retrieve_bid");
 		});
 	});
 }
+
+
+export function efect_price_logic (operation,amount,limit_price,depth,data) {
+	
+	var amount_limit=0;
+	var amount_limit_value=0;
+	var acumulate=0;
+	var acumulate_value=0;
+	/** Data have all the information for bid and ask**/
+	if (operation==='sell')	{
+		data=data[0];
+	}
+	if (operation==='buy')	{
+		data=data[1];	
+	}	
+
+	for (let i = 0; i <depth; i++) {
+		if (amount-acumulate >0) {
+			if (amount-acumulate > data[i][1]) {
+				acumulate_value+=data[i][0]*data[i][1];
+			}else {
+				acumulate_value+=data[i][0]*(amount-acumulate);
+			}
+			acumulate+=data[i][1];
+		}
+		
+		if (limit_price<data[i][0] && operation==='sell') {
+			amount_limit+=data[i][1];
+			amount_limit_value+=data[i][0]*data[i][1];
+			}
+		if (limit_price>data[i][0] && operation==='buy') {
+			amount_limit+=data[i][1];
+			amount_limit_value+=data[i][0]*data[i][1];
+			}
+	}
+	return [acumulate_value,amount_limit,amount_limit_value];
+}
+
 
 
 //Fetches a list of valid exchange traiding pairs
